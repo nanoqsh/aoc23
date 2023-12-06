819c97dcacde72fb53fd46360828c26674e24495
nanoqsh <nanoqsh@gmail.com>
2023-12-06 08:13:18 +0600

    Refactor tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,26 +2,45 @@ pub mod day1;
 pub mod day2;
 pub mod day3;
 pub mod day4;
+pub mod day5;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn all() {
-        const TASKS: [(u8, u8, fn(&str) -> u32, &str, u32); 8] = [
-            (1, 1, day1::part1, include_str!("day1"), 53080),
-            (1, 2, day1::part2, include_str!("day1"), 53268),
-            (2, 1, day2::part1, include_str!("day2"), 2256),
-            (2, 2, day2::part2, include_str!("day2"), 74229),
-            (3, 1, day3::part1, include_str!("day3"), 556367),
-            (3, 2, day3::part2, include_str!("day3"), 89471771),
-            (4, 1, day4::part1, include_str!("day4"), 21821),
-            (4, 2, day4::part2, include_str!("day4"), 5539496),
-        ];
+    fn day1() {
+        let output = day1::part1(include_str!("day1"));
+        assert_eq!(output, 53080);
 
-        for (day, part, task, input, expected) in TASKS {
-            assert_eq!(task(input), expected, "day {day} part {part}");
-        }
+        let output = day1::part2(include_str!("day1"));
+        assert_eq!(output, 53268);
+    }
+
+    #[test]
+    fn day2() {
+        let output = day2::part1(include_str!("day2"));
+        assert_eq!(output, 2256);
+
+        let output = day2::part2(include_str!("day2"));
+        assert_eq!(output, 74229);
+    }
+
+    #[test]
+    fn day3() {
+        let output = day3::part1(include_str!("day3"));
+        assert_eq!(output, 556367);
+
+        let output = day3::part2(include_str!("day3"));
+        assert_eq!(output, 89471771);
+    }
+
+    #[test]
+    fn day4() {
+        let output = day4::part1(include_str!("day4"));
+        assert_eq!(output, 21821);
+
+        let output = day4::part2(include_str!("day4"));
+        assert_eq!(output, 5539496);
     }
 }
